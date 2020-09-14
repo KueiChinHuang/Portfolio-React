4527450b2374630eb08fc8ceaff8b8481c0d5940
Kuei-Chin Huang <Kuei-Chin.Huang@MyGeorgian.ca>
2020-09-14 11:43:16 -0400

    fancy appear animation for nav bar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import FlipMove from "react-flip-move";
 
 class Header extends Component {
   render() {
@@ -29,36 +30,38 @@ class Header extends Component {
           </a>
 
           <ul id="nav" className="nav">
-            <li className="current">
-              <a className="smoothscroll" href="#home">
-                Home
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#about">
-                About
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#resume">
-                Resume
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#portfolio">
-                Works
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#testimonials">
-                Testimonials
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#contact">
-                Contact
-              </a>
-            </li>
+            <FlipMove staggerDelayBy={150} appearAnimation="accordionVertical">
+              <li key="1" className="current">
+                <a className="smoothscroll" href="#home">
+                  Home
+                </a>
+              </li>
+              <li key="2">
+                <a className="smoothscroll" href="#about">
+                  About
+                </a>
+              </li>
+              <li key="3">
+                <a className="smoothscroll" href="#resume">
+                  Resume
+                </a>
+              </li>
+              <li key="4">
+                <a className="smoothscroll" href="#portfolio">
+                  Works
+                </a>
+              </li>
+              <li key="5">
+                <a className="smoothscroll" href="#testimonials">
+                  Testimonials
+                </a>
+              </li>
+              <li key="6">
+                <a className="smoothscroll" href="#contact">
+                  Contact
+                </a>
+              </li>
+            </FlipMove>
           </ul>
         </nav>
 
