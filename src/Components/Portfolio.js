86ad81d9b148da74abb6965e7aa4848788402f88
Kuei-Chin Huang <kuei-chin.huang@mygeorgian.ca>
2020-11-05 08:00:55 -0500

    update works

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -13,7 +13,8 @@ class Portfolio extends Component {
                 <div className="overlay">
                   <div className="portfolio-item-meta">
                     <h5>{projects.title}</h5>
-                    <p>{projects.category}</p>
+                    <p>{projects.description}</p>
+                    <p>Built with <span>{projects.skill}</span></p>
                   </div>
                 </div>
                 <div className="link-icon">
