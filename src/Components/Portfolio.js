b34519194e39fe88fe530040fdd551e0c242b004
Kuei-Chin Huang <kuei-chin.huang@mygeorgian.ca>
2020-11-05 11:13:52 -0500

    try to remove 2 p

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -13,7 +13,6 @@ class Portfolio extends Component {
                 <div className="overlay">
                   <div className="portfolio-item-meta">
                     <h5>{projects.title}</h5>
-                    <p>{projects.description}</p>
                     <p>Built with <span>{projects.skill}</span></p>
                   </div>
                 </div>
