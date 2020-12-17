29bd10b38f4bc59263ddd85299f4ede53ba2b191
Kuei-Chin Huang <kuei-chin.huang@mygeorgian.ca>
2020-12-16 19:32:24 -0500

    UPDATE: about

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,35 +1,39 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 class About extends Component {
   render() {
     if (this.props.data) {
-      var name = this.props.data.name;
-      var profilepic = "images/" + this.props.data.image;
-      var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone = this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
+      var name = this.props.data.name
+      var profilepic = 'images/' + this.props.data.image
+      var bio = this.props.data.bio
+      var bio2 = this.props.data.bio2
+      var bio3 = this.props.data.bio3
+      var street = this.props.data.address.street
+      var city = this.props.data.address.city
+      var state = this.props.data.address.state
+      var zip = this.props.data.address.zip
+      var phone = this.props.data.phone
+      var email = this.props.data.email
+      var resumeDownload = this.props.data.resumedownload
     }
 
     return (
-      <section id="about">
-        <div className="row">
-          <div className="three columns">
+      <section id='about'>
+        <div className='row'>
+          <div className='three columns'>
             <img
-              className="profile-pic"
+              className='profile-pic'
               src={profilepic}
-              alt="Kuei-Chin Huang Profile Pic"
+              alt='Kuei-Chin Huang Profile Pic'
             />
           </div>
-          <div className="nine columns main-col">
+          <div className='nine columns main-col'>
             <h2>About Me</h2>
 
             <p>{bio}</p>
-            <div className="row">
+            <p>{bio2}</p>
+            <p>{bio3}</p>
+            <div className='row'>
               {/* <div className="columns contact-details">
                 <h2>Contact Details</h2>
                 <p className="address">
@@ -46,10 +50,10 @@ class About extends Component {
                   <span>{email}</span>
                 </p>
               </div> */}
-              <div className="columns download">
+              <div className='columns download'>
                 <p>
-                  <a href={resumeDownload} className="button" download>
-                    <i className="fa fa-download"></i>Download Resume
+                  <a href={resumeDownload} className='button' download>
+                    <i className='fa fa-download'></i>Download Resume
                   </a>
                 </p>
               </div>
@@ -57,8 +61,8 @@ class About extends Component {
           </div>
         </div>
       </section>
-    );
+    )
   }
 }
 
-export default About;
+export default About
