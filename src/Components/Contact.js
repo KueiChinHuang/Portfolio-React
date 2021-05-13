ed3cea0a63cf12af20efc514d2ada64adbd22ddf
Kuei-Chin Huang <kuei-chin.huang@fluent.ai>
2021-05-13 18:16:44 -0400

    update resume and pics

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -139,22 +139,6 @@ const Contact = ({ data }) => {
             <br />
           </div>
         </div>
-
-        {/* <aside className="four columns footer-widgets">
-          <div className="widget widget_contact">
-            <h4>Address and Phone</h4>
-            <p className="address">
-              {data?.name}
-              <br />
-              {data?.address.street} <br />
-              {data?.address.city}, {data?.address.state} {data?.address.zip}
-              <br />
-              <span>{data?.phone}</span>
-            </p>
-          </div>
-
-          <div className="widget widget_tweets"></div>
-        </aside> */}
       </div>
     </section>
   );
